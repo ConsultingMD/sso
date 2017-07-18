fb03b5aa7da2b5b9011d9ee73c5d75d74b2b440b
Sandip Srinivas <sandip.srinivas@grandrounds.com>
2017-07-18 15:41:41 -0700

    Removed singleton, allowed for creation of instance with specified role

--- lib/grnds/sso/authorization_constraint.rb
+++ lib/grnds/sso/authorization_constraint.rb
@@ -1,18 +1,16 @@
 module Grnds::Sso
   class AuthorizationConstraint
-  	include Singleton
-  	
-  	class << self
-      delegate :configure, :matches?, to: :instance
-    end
 
-    def configure
-      yield self
+    attr_accessor :role
+
+    def initialize(role)
+      self.role = role
     end
 
     def matches?(request)
       session = request.session
-      return session['primary_role'].eql?('admin')
+      return false if session['uid'].eql?(nil)
+      return session['primary_role'].eql?(self.role)
     end
   end
 end
