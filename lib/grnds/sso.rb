e1b2d08074ac809298844aba652546d1da7f7786
Kartik Subramanian <kartik@grandrounds.com>
2019-07-19 14:01:45 -0700

    Authorization implementation

--- lib/grnds/sso.rb
+++ lib/grnds/sso.rb
@@ -2,5 +2,6 @@ require 'grnds/sso/version'
 require 'grnds/sso/configuration'
 require 'grnds/sso/view_helpers' if defined?(Rails)
 require 'grnds/sso/authentication' if defined?(Rails)
+require 'grnds/sso/authorization' if defined?(Rails)
 require 'grnds/sso/vpn_constraint' if defined?(Rails)
 require 'grnds/sso/authorization_constraint' if defined?(Rails)
