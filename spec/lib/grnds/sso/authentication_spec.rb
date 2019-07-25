007eb9878ec24f7cdcd43de9e53ca2a5156bb450
Gaurav Sharma <gaurav.qwe@gmail.com>
2019-07-25 14:36:19 -0700

    fix tests

--- spec/lib/grnds/sso/authentication_spec.rb
+++ spec/lib/grnds/sso/authentication_spec.rb
@@ -2,6 +2,7 @@ describe Grnds::Sso::Authentication do
   subject do
     Class.new do
       def session; end
+      def redirect_to(url); end
       def self.helper_method(method); end
 
       # testing protected methods and everything in this module is protected
@@ -15,6 +16,7 @@ describe Grnds::Sso::Authentication do
 
   before do
     allow(subject).to receive(:session).and_return(OpenStruct.new(session))
+    allow(subject).to receive(:redirect_to)
   end
 
   let(:session) {{}}
@@ -29,8 +31,25 @@ describe Grnds::Sso::Authentication do
   end
 
   describe '#authenticate_user' do
-    it do
+    before do
+      Grnds::Sso.configure do |config|
+        config.base_site = 'http://grnds.com'
+        config.sign_in_post_fix = '/login'
+      end
+    end
+
+    it 'redirects to login page' do
       subject.authenticate_user
+      expect(subject).to have_received(:redirect_to).with('http://grnds.com/login?return_to=http%3A%2F%2Ftest.host%2F')
+    end
+
+    context 'when authenticated' do
+      let(:session) {{ 'uid'=> 1 }}
+
+      it 'doesnt redirect' do
+        subject.authenticate_user
+        expect(subject).not_to have_received(:redirect_to)
+      end
     end
   end
 end
