20ff2888862414eaa7ebf51f3a48ea40a9547c86
Kartik Subramanian <kartik@grandrounds.com>
2019-07-29 12:36:44 -0700

    Remove helper_method because we're not doing any view stuff here

--- lib/grnds/sso/authorization.rb
+++ lib/grnds/sso/authorization.rb
@@ -3,10 +3,6 @@ module Grnds
     module Authorization
       extend ActiveSupport::Concern
 
-      included do
-        helper_method :authorized_for?
-      end
-
       def authorized_for?(request, roles)
         AuthorizationConstraint.new(roles).matches?(request)
       end
